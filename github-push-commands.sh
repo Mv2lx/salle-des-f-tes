88631a061ae7176ab3e39c5186104d0d3b41270b
MV2LX <chmaafip@gmail.com>
2026-07-23 08:14:09 +0200

    Initial commit

--- github-push-commands.sh
+++ github-push-commands.sh
@@ -6,5 +6,5 @@ git init
 git add .
 git commit -m "Initial commit"
 git branch -M main
-git remote add origin https://github.com/Mv2lx/salle-des-f-tes
+git remote add origin https://github.com/Mv2lx/Elfares-Salle
 git push -u origin main
